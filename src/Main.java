52d11321e3b4b8b6ff46122d74ef9e306f74cdd4
ArthDroid1 <barbosarthur7@gmail.com>
2026-02-18 13:44:22 -0300

    Finish exercise

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,12 @@ public class Main {
 		System.out.println("Enter the number of products: ");
 		int n = sc.nextInt();
 		
-		for (int i = 0; i <= n; i++) {
-			System.out.printf("Product #%d", n);
+		for (int i = 1; i < n; i++) {
+			System.out.printf("Product #%d data: \n", i + 1);
 			System.out.println("Common, Used or Imported (c/u/i)");
 			char option = sc.next().charAt(0);
 			System.out.println("Name: ");
+			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.println("Price: ");
 			double price = sc.nextDouble();
@@ -40,6 +41,7 @@ public class Main {
 			}
 			if (option == 'u') {
 				System.out.println("Manufacture Date (DD/MM/YYYY): ");
+				sc.nextLine();
 				Date manufactureDate = sdf.parse(sc.nextLine());
 				UsedProduct usedProduct = new UsedProduct(name,price, manufactureDate);
 				products.add(usedProduct);
