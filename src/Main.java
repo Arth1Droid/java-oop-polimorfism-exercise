01e6c22b6337711b9dce18f774d054f35a52216c
ArthDroid1 <barbosarthur7@gmail.com>
2026-02-18 13:31:07 -0300

    Create Main class

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> products = new ArrayList<>();
+		
+		System.out.println("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			System.out.printf("Product #%d", n);
+			System.out.println("Common, Used or Imported (c/u/i)");
+			char option = sc.next().charAt(0);
+			System.out.println("Name: ");
+			String name = sc.nextLine();
+			System.out.println("Price: ");
+			double price = sc.nextDouble();
+			
+			Product communProduct = new Product(name,price);
+			products.add(communProduct);
+			
+			if (option == 'i') {
+				System.out.print("Customs fee: ");
+				double customsFee = sc.nextDouble();
+				ImportedProduct importedProduct = new ImportedProduct(name,price,customsFee);
+				products.add(importedProduct);
+
+			}
+			if (option == 'u') {
+				System.out.println("Manufacture Date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.nextLine());
+				UsedProduct usedProduct = new UsedProduct(name,price, manufactureDate);
+				products.add(usedProduct);
+			}
+			
+			
+		}
+		
+		System.out.println("PRICE TAGS: ");
+		
+		for (Product product : products) {
+			System.out.println(product);
+		}
+		
+
+	}
+
+}
